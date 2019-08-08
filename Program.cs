0d679735b8c7e67aae1cb5632e6f45bac99578ca
William <harv.wc@gmail.com>
2019-08-08 10:22:53 -0400

    Added the recursive function and some fun text. :)

--- Program.cs
+++ Program.cs
@@ -8,7 +8,29 @@ namespace Staircase_Problem
         //ie: N=4; 5 unique ways.
         static void Main(string[] args)
         {
-           
+            CountingStairs Stairs = new CountingStairs { };
+            Console.Write("How many stairs are we climbing today? ");
+            long n = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine($"Awesome! Why not make it interesting and climb the {n} stairs all {Stairs.UniqueWays(n)} different ways?");
+            
+            // Don't enter more than like 35-ish... Or do; it's whatever... //
+
+            //PAUSE//
+            Console.ReadLine();
+        }
+    }
+    class CountingStairs
+    {
+        public long UniqueWays(long n)
+        {
+            switch (Math.Abs(n))
+            {
+                case 0:
+                case 1:
+                    return 1;
+                default:
+                    return (UniqueWays(n - 1) + UniqueWays(n - 2));
+            }
         }
     }
 }
