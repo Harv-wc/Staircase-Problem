9e95c5d368e75c82429dfae1fe7e9fb4097dc464
William <harv.wc@gmail.com>
2019-08-08 09:09:37 -0400

    empty

--- Program.cs
+++ Program.cs
@@ -3,7 +3,9 @@
 namespace Staircase_Problem
 {
     class Program
-    {
+    {   //There exists a staircase with N steps, and you can climb 1 or 2 steps at a time.
+        //Given N, write a function to return the number of unique ways to climb the staircase.
+        //ie: N=4; 5 unique ways.
         static void Main(string[] args)
         {
            
